2e5eb633371bdccaca30ee4f6df397386aa4695b
Xinyue Hao <45228737+maggieHao@users.noreply.github.com>
2025-04-23 17:13:50 +0100

    Create run_main_pretrain.sh

---
+++ run_main_pretrain.sh
@@ -0,0 +1,9 @@
+python -m torch.distributed.launch --nproc_per_node=2 main_pretrain.py  --batch_size 16 \
+    --world_size 2 \
+    --accum_iter 4 \
+    --model mae_vit_base_patch16 \
+    --norm_pix_loss \
+    --mask_ratio 0.75 \
+    --epochs 800 \
+    --warmup_epochs 40 \
+    --blr 1.5e-4 --weight_decay 0.05
