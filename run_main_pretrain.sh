7b2bcae3afda8335be046c3ba86344bbc2e91748
Xinyue Hao <45228737+maggieHao@users.noreply.github.com>
2025-04-26 23:04:33 +0100

    Update run_main_pretrain.sh

--- run_main_pretrain.sh
+++ run_main_pretrain.sh
@@ -1,10 +1,11 @@
-python -m torch.distributed.launch --nproc_per_node=2 main_pretrain.py  --batch_size 128 \
-    --world_size 2 \
-    --accum_iter 4 \
+python main_pretrain.py \
+    --batch_size 512 \
+    --world_size 1 \
     --model mae_vit_base_patch16 \
     --norm_pix_loss \
     --mask_ratio 0.75 \
     --epochs 800 \
     --warmup_epochs 40 \
-    --data_path '/home/maggie/Documents/ImageNet1k'\
-    --blr 1.5e-4 --weight_decay 0.05
+    --data_path '/mnt/ceph/mllm_task/dataset' \
+    --blr 1.5e-4 \
+    --weight_decay 0.05
